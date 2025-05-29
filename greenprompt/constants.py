9cc65661f7bcf8248d107889f99ce7179107ca2b
Uday <udapra@gmail.com>
2025-05-28 22:33:37 -0400

    Untrack constants.py so it's only ignored and never committed after setup

--- greenprompt/constants.py
+++
@@ -1,8 +0,0 @@
-# greenprompt/constants.py
-# This file is auto-overwritten by setup.py. Do not edit or commit real values!
-OS = ""
-OS_VERSION = ""
-PLATFORM = ""
-MACHINE = ""
-PROCESSOR = ""
-OLLAMA_URL = "http://127.0.0.1:11434"
